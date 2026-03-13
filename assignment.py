00d5e7f6f5893b57989f5075601d50371d2f985e
Joy Cindy <allerendic254@gmail.com>
2026-03-13 19:24:52 +0300

    Update assignment.py
    Added multiple print statements and data structures for questions 4 to 7.

--- assignment.py
+++ assignment.py
@@ -1,1 +1,30 @@
+# QUESTION 4
+print("Hello AI Students")
 
+# QUESTION 5
+Name= "John"
+Age= 20
+City= "Nairobi"
+print("Name:",Name)
+#Name: John
+print("Age:",Age)
+#Age: 20
+print("City:",City)
+#City: Nairobi
+
+# QUESTION 6
+languages= ["Python","Java","C++","JavaScript","Go"]
+print(languages[0])
+#Output- python
+# QUESTION 7
+student= {
+  "Name":"Blessy",
+  "Age":23,
+  "Course":"AI"
+}
+print(student["Name"])
+#Output- Blessy
+
+# Bonus question
+# Difference between a list and a dictionary.
+# A list is a built-in data structure used to store a collection of items in a single variable whereas a dictionary is a built-in data structure used to store data in key-value pairs.
